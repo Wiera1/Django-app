54fc1207aab3ce95108cc9acb9f5fc4e0dc94066
Wiera1 <mr.veziryan1@yandex.ru>
2025-10-24 14:42:02 +0300

    poetry in dockerfile

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -78,19 +78,16 @@ INSTALLED_APPS = [
     'django.contrib.admindocs',
     'django.contrib.sitemaps',
 
-    #'debug_toolbar',
     'rest_framework',
     'django_filters',
     'drf_spectacular',
 
     'shopapp.apps.ShopappConfig',
     'myauth.apps.MyauthConfig',
-    'myapiapp.apps.MyapiappConfig',
     'blogapp.apps.BlogappConfig',
 ]
 
 MIDDLEWARE = [
-    # 'django.middleware.cache.UpdateCacheMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -99,8 +96,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.contrib.admindocs.middleware.XViewMiddleware',
-   # 'debug_toolbar.middleware.DebugToolbarMiddleware',
-    # 'django.middleware.cache.FetchFromCacheMiddleware',
 ]
 
 ROOT_URLCONF = 'mysite.urls'
